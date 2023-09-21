66a557663f9e6ef2ddeaf7f45893ac45c7f7fb7a
christian-hopf <christian.hopf@cognizant.com>
2023-09-21 16:09:48 -0500

    Base spring project

---
+++ src/main/java/com/example/droolspoc/DroolspocApplication.java
@@ -0,0 +1,13 @@
+package com.example.droolspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolspocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroolspocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/droolspoc/DroolspocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.droolspoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolspocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
